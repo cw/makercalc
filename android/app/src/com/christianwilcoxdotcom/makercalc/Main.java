38b7b33b52c37a739e39f377426d77e51a27606c
Christian Wilcox <cw@christianwilcox.com>
2011-08-30 18:50:10 -0500

    added initial webview to Main activity; load makercalc.appspot.com

--- android/app/src/com/christianwilcoxdotcom/makercalc/Main.java
+++ android/app/src/com/christianwilcoxdotcom/makercalc/Main.java
@@ -2,12 +2,18 @@ package com.christianwilcoxdotcom.makercalc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    WebView myWebView = (WebView) findViewById(R.id.webview);
+    WebSettings webSettings = myWebView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    myWebView.loadUrl("http://makercalc.appspot.com");
+  }
 }
