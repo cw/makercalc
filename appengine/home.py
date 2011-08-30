f373c1e2638aa79ac68882dc978e21b23566670a
Christian Wilcox <cw@christianwilcox.com>
2011-08-30 10:10:58 -0500

    initial plain html template

---
+++ appengine/home.py
@@ -0,0 +1,25 @@
+import os
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    greetings = 'Hello, makers!'
+    template_values = {
+      'greetings': greetings,
+      'url': "url woo!",
+      'url_linktext': "url link text",
+    }
+    path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+    self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
