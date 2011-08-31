6f3797eb83d56a40eb54d8db978df1b385bf106e
Christian Wilcox <cw@christianwilcox.com>
2011-08-30 23:33:26 -0500

    commented out Intent import. at some point will add this back

--- android/app/src/com/christianwilcoxdotcom/makercalc/Main.java
+++ android/app/src/com/christianwilcoxdotcom/makercalc/Main.java
@@ -1,7 +1,7 @@
 package com.christianwilcoxdotcom.makercalc;
 
 import android.app.Activity;
-import android.content.Intent;
+//import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
