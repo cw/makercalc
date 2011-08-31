1b8d07b86da9a7c9f32d2a8b45e91f00087af695
Christian Wilcox <cw@christianwilcox.com>
2011-08-30 22:19:18 -0500

    added adapt.js and related assets; added zepto.js

--- android/app/src/com/christianwilcoxdotcom/makercalc/Main.java
+++ android/app/src/com/christianwilcoxdotcom/makercalc/Main.java
@@ -1,9 +1,26 @@
 package com.christianwilcoxdotcom.makercalc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+class MyWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().equals("makercalc.appspot.com")) {
+            // This is my web site, so do not override; let my WebView load the page
+            return false;
+        }
+        // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        //startActivity(intent);
+        return true;
+    }
+}
 
 public class Main extends Activity {
   /** Called when the activity is first created. */
@@ -12,11 +29,11 @@ public class Main extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     WebView myWebView = (WebView) findViewById(R.id.webview);
+    myWebView.setWebViewClient(new MyWebViewClient());
     WebSettings webSettings = myWebView.getSettings();
     webSettings.setJavaScriptEnabled(true);
     
     String ua = webSettings.getUserAgentString();
-    
     webSettings.setUserAgentString(ua + "; makercalc_android_app");
     myWebView.loadUrl("http://makercalc.appspot.com");
   }
