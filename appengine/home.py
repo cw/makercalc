f821cea87dcf077685431e0ad7bc7816330c35e0
Christian Wilcox <cw@christianwilcox.com>
2012-02-14 22:28:24 -0600

    corrected WSGIApplication url mapping

--- appengine/home.py
+++ appengine/home.py
@@ -18,8 +18,24 @@ class MainPage(webapp.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
     self.response.out.write(template.render(path, template_values))
 
+class MathPage(webapp.RequestHandler):
+  def get(self):
+    user_agent = str(self.request.headers['User-Agent'])
+    source = "web"
+    if "makercalc_android_app" in user_agent:
+      source = "Android"
+    greetings = 'Hello, %s makers!' % (source,)
+    template_values = {
+      'greetings': greetings,
+      'url': "url woo!",
+      'url_linktext': "url link text",
+    }
+    path = os.path.join(os.path.dirname(__file__), 'templates/math.html')
+    self.response.out.write(template.render(path, template_values))
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+                                      ('/math', MathPage)],
                                      debug=True)
 
 def main():
